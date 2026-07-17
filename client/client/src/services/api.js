0988027ece7520a8cadb1c60cda37dadd2d561a4
Devesh Vijay Patil <deveshpatil450@gmail.com>
2026-07-17 16:41:14 +0530

    Update API URL for production

--- client/client/src/services/api.js
+++ client/client/src/services/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// const api = axios.create({
+//     baseURL: "http://localhost:5000/api"
+// });
+
+
+
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+  baseURL: "https://customer-support-crm-application.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
